239499ba2d7411fe5658c750bde3f967e80fa13e
jokerhacker22 <53850334+jokerhacker22@users.noreply.github.com>
2020-01-25 05:52:27 -0500

    Add files via upload

---
+++ WhatsappBomber.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+import os
+
+os.system("clear")
+
+print("\033[1;32;40m ░░░░░██╗░█████╗░██╗░░██╗███████╗██████╗░  ██╗░░██╗░█████╗░░█████╗░██╗░░██╗███████╗██████╗░")
+print("\033[1;32;40m ░░░░░██║██╔══██╗██║░██╔╝██╔════╝██╔══██╗  ██║░░██║██╔══██╗██╔══██╗██║░██╔╝██╔════╝██╔══██╗")
+print("\033[1;32;40m ░░░░░██║██║░░██║█████═╝░█████╗░░██████╔╝  ███████║███████║██║░░╚═╝█████═╝░█████╗░░██████╔╝")
+print("\033[1;32;40m ██╗░░██║██║░░██║██╔═██╗░██╔══╝░░██╔══██╗  ██╔══██║██╔══██║██║░░██╗██╔═██╗░██╔══╝░░██╔══██╗")
+print("\033[1;32;40m ╚█████╔╝╚█████╔╝██║░╚██╗███████╗██║░░██║  ██║░░██║██║░░██║╚█████╔╝██║░╚██╗███████╗██║░░██║")
+print("\033[1;32;40m ░╚════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝  ╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝\n")
+print("                            This Script Was Made By JOKER HACKER​​​​​                          ")
+
+path = input("Enter the path location for geckodriver: ")
+driver = webdriver.Firefox(executable_path=path)
+driver.get('https://web.whatsapp.com/')
+
+name = input('Enter the name of user or group : ')
+msg = input('Enter your message : ')
+count = int(input('Enter the count : '))
+
+input('Enter anything after scanning QR code')
+
+user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+user.click()
+
+msg_box = driver.find_element_by_class_name('_3u328')
+
+for i in range(count):
+    msg_box.send_keys(msg)
+    button = driver.find_element_by_class_name('hnQHL')
+    button.click()
